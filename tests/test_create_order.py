27b6f82878d713de556acf5dbabecfadcbbecd0b
Nuikin Denis <dnsurgai@yandex.ru>
2024-07-18 15:17:25 +0400

    add description for allure

--- tests/test_create_order.py
+++ tests/test_create_order.py
@@ -7,27 +7,27 @@ from http import HTTPStatus
 @allure.suite("Создание заказа")
 class TestCreateOrder:
 
-    @allure.description("")
+    @allure.description("Создание заказа авторизованным юзером сопровождается ответом 200")
     @allure.title("Создание заказа авторизованным пользователем")
     def test_create_order_with_auth(self, create_user):
         token = {'Authorization': create_user[3]}
         r = requests.post(f"{Urls.MAIN_URL}{Handlers.MAKE_ORDER}", headers=token, data=Ingredient.correct_ingredients_data)
         assert r.status_code == HTTPStatus.OK.value and r.json().get("success") is True
 
-    @allure.description("")
+    @allure.description("Создание заказа не авторизованным юзером сопровождается ответом 200")
     @allure.title("Создание заказа не авторизованным пользователем")
     def test_create_order_not_auth(self):
         r = requests.post(f"{Urls.MAIN_URL}{Handlers.MAKE_ORDER}", data=Ingredient.correct_ingredients_data)
         assert r.status_code == HTTPStatus.OK.value and r.json().get("success") is True
 
 
-    @allure.description("")
+    @allure.description("Создание пустого заказа сопровождается ответом 400")
     @allure.title("Создание заказа без ингредиентов")
     def test_create_order_with_ingridient(self):
         r = requests.post(f"{Urls.MAIN_URL}{Handlers.MAKE_ORDER}")
         assert r.status_code == HTTPStatus.BAD_REQUEST.value and r.json()['message'] == "Ingredient ids must be provided"
 
-    @allure.description("")
+    @allure.description("Создание с невалидным хешем ингредиента сопровождается ошибкой 500")
     @allure.title("Создание с невалидным хешем ингредиента")
     def test_create_order_invalid_hash_ingridient(self):
         response = requests.post(Urls.MAIN_URL + Handlers.MAKE_ORDER, headers=Handlers.headers,
--- tests/test_get_order_for_user.py
+++ tests/test_get_order_for_user.py
@@ -8,7 +8,7 @@ from http import HTTPStatus
 @allure.suite("Получение доступных заказов по пользователю")
 class TestGetOrderUser:
 
-    @allure.description("")
+    @allure.description("Получение доступных заказов авторизованного пользователя - ответ 200")
     @allure.title("Получение доступных заказов авторизованного пользователя")
     def test_get_order_user_with_auth(self, create_user):
         token = {'Authorization': create_user[3]}
@@ -17,7 +17,7 @@ class TestGetOrderUser:
         assert response_get_order.status_code == HTTPStatus.OK.value and response_get_order.json()['orders'][0]['number'] == requests_create_order.json()['order']['number']
 
 
-    @allure.description("")
+    @allure.description("Получение заказов пользователя если пользователь не авторизовался - ответ 401")
     @allure.title("Получение заказов пользователя если пользователь не авторизовался")
     def test_get_order_user_not_auth(self):
         r = requests.get(f"{Urls.MAIN_URL}{Handlers.GET_ORDERS}")
